5cecec65243ab2426a976233409ae540034e35b0
Marco Maggi <marco.maggi-ipsu@poste.it>
2020-04-13 15:35:44 +0200

    more identifiers for font locking

--- src/cclibraries-addon-lists-of-symbols.el
+++ src/cclibraries-addon-lists-of-symbols.el
@@ -4,7 +4,7 @@
 
 ;; Author: Marco Maggi <mrc.mgg@gmail.com>
 ;; Created: Mar 22, 2020a
-;; Time-stamp: <2020-04-12 10:43:15 marco>
+;; Time-stamp: <2020-04-13 12:37:25 marco>
 ;; Keywords: convenience, data, languages
 
 ;; This file is part of MMUX Emacs CCLibraries Addon.
@@ -88,6 +88,9 @@
     "cclib_resource_pointer"
     ;;
     "cclib_trait_resource_pointer"
+    ;;
+    "cclib_handler_type"
+    "cclib_init_and_register_handler"
     ;;
     "cclib_fun"
     "cclib_is"
